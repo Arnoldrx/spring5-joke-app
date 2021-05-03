3c5f0d17680032cf503d027198af00563b52a0b0
Tagne  Paul Arnold <tagnearnold2080@gmail.com>
2021-05-03 16:26:40 +0100

    Create spring boot Project

---
+++ src/test/java/guru/springframework/spring5jokeapp/Spring5JokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
